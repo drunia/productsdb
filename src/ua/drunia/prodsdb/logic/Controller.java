6a32b65ff40960c91990c720cda50445008a8945
drunia <drunia@xakep.ru>
2013-09-25 12:05:24 +0300

    add Controller.java

---
+++ src/ua/drunia/prodsdb/logic/Controller.java
@@ -0,0 +1,37 @@
+/**
+ * Abstract controller class
+ * @auhor drunia
+ * @since 25.09.2013
+ */
+package ua.drunia.prodsdb.logic;
+
+public abstract class Controller {
+	private Database db;
+	private IUserUI ui;
+	/**
+	 * Default constructor
+	 * @param db - initialized database
+	 * @param ui - controlled view
+	 * @author drunia
+	 */
+	 public Controller(Database db, IUserUI ui) {
+		this.db = db;
+		this.ui = ui;
+	 }
+	/**
+	 * Set initialized database
+	 * @param db - initialized database
+	 * @author drunia
+	 */
+	protected void setDatabase(Database db) {
+		this.db = db;
+	}
+	/**
+	 * Set controlled view
+	 * @param ui - controled view
+	 * @author drunia
+	 */
+	protected void setView(IUserUI ui) {
+		this.ui = ui;
+	}
+}
